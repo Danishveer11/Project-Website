d92e6aec5c9c2c8ac63ea065d1e4c3885a18348e
Danishveer Thiara <danishveerthiara@gmail.com>
2026-01-27 10:47:58 -0800

    Updated file

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+# wait 5 seconds for DB to be ready
+sleep 5
+
+# Run migrations
 python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Start Gunicorn
 exec gunicorn Project.wsgi
